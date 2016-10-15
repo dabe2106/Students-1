3b8da44f08c896d5095500f6b1f36139113abd1a
Dave Del Rosario <davedelrosario37@gmail.com>
2016-10-15 12:57:24 +0800

    Add files via upload

--- ACT/Del-Rosario-Dave/bubble-sort/BubbleSort.java
+++ ACT/Del-Rosario-Dave/bubble-sort/BubbleSort.java
@@ -1,53 +1,36 @@
-/**
-* BUBBLE SORT is based on the idea of repeatedly comparing pairs of 
-* adjacent elements, then switching positions if they exist in the wrong order.
-*
-* @author  [your full name here]
-* @link    [full github url]
-* @version [version number]
-* @since   [last updated dd/mm/yyyy]
-*/
+import java.util.Scanner;
 
-// TODO: import Scanner and Random from Java library
-
-public class BubbleSort {
-
-	public static void main(String[] args) {
-
-		int array[], size, x, y, swap;
-
-		Scanner input = new Scanner(System.in);
-		Random rand = new Random();
-
-		System.out.print("Enter array size to sort: ");
-		// TODO: use nextInt() to assign keyboard input as array size
-		array = new int[size];
-
-		System.out.println();
-		System.out.println(size + " random numbers generated.");
-
-		for (x = 0; x < size; x++) {
-			// TODO: use nextInt() to assign random numbers (range of 1000) to array index
-			// TODO: print unsorted numbers separated by space
-		}
-
-		System.out.println();
-		// note: compare each from the first index until largest number is moved to the last index.
-		for (x = 0; x < size-1; x++) { // note: loop until before the end of array
-			for (y = 0; y < size-x-1; y++) { // note: will not loop to numbers already sorted
-				if (array[y] > array[y+1]) { // note: compare adjacent numbers which is greater
-					swap = array[y]; // note: swap positions...
-					array[y] = array[y+1]; // note: smaller will be move to the left
-					array[y+1] = swap; // note: larger will move to the right
-				}
-			}
-		}
-
-		System.out.println();
-
-		System.out.println("Bubble Sorting... Done!");
-		
-		// TODO: print sorted numbers separated by space
+public class Bubblesort {
+	
+	
+	  public static void main(String []args) {
+	    int num, i, j, temp;
+	    Scanner input = new Scanner(System.in);
+	 
+	    System.out.println("Enter the number of integers to sort:");
+	    num = input.nextInt();
+	 
+	    int array[] = new int[num];
+	 
+	    System.out.println("Enter " + num + " integers: ");
+	 
+	    for (i = 0; i < num; i++) 
+	      array[i] = input.nextInt();
+	 
+	    for (i = 0; i < ( num - 1 ); i++) {
+	      for (j = 0; j < num - i - 1; j++) {
+	        if (array[j] > array[j+1]) 
+	        {
+	           temp = array[j];
+	           array[j] = array[j+1];
+	           array[j+1] = temp;
+	        }
+	      }
+	    }
+	 
+	    System.out.println("Sorted list of integers:");
+	 
+	    for (i = 0; i < num; i++) 
+	      System.out.println(array[i]);
+	  }
 	}
-
-}
